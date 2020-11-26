5bf6e9592f7f4c773e7684e5a22c243d757cc6ed
Didier Gautheron <didier.gautheron@biblibre.com>
2020-11-26 10:41:35 +0100

    Add intranet_catalog_biblio_tab's tab new method

--- Koha/Plugin/Com/BibLibre/LinkedData.pm
+++ Koha/Plugin/Com/BibLibre/LinkedData.pm
@@ -3,8 +3,11 @@ package Koha::Plugin::Com::BibLibre::LinkedData;
 use base qw(Koha::Plugins::Base);
 
 use Modern::Perl;
+use RDF::Query::Client; 
 
 use C4::Context;
+use Koha::Plugins::Tab;
+
 
 our $VERSION = '0.1';
 
@@ -67,4 +70,25 @@ sub tool {
     return $self->output_html( $template->output() );
 }
 
+sub intranet_catalog_biblio_tab {                                                                                                                           
+    my @tabs;                                                                                                                                               
+    push @tabs,                                                                                                                                             
+      Koha::Plugins::Tab->new(                                                                                                                              
+        {                                                                                                                                                   
+            title   => 'WikiData',                                                                                                                            
+            content => 'This is content for tab 1'                                                                                                          
+        }                                                                                                                                                   
+      );                                                                                                                                                    
+                                                                                                                                                            
+    push @tabs,                                                                                                                                             
+      Koha::Plugins::Tab->new(                                                                                                                              
+        {                                                                                                                                                   
+            title   => 'Tab 2',                                                                                                                             
+            content => 'This is content for tab 2'                                                                                                          
+        }                                                                                                                                                   
+      );                                                                                                                                                    
+                                                                                                                                                            
+    return @tabs;                                                                                                                                           
+}                                                                                                                                                           
+   
 1;
