19d751cbe6c640dae640aad0e158b83494c9c792
Fridolin Somers <fridolin.somers@biblibre.com>
2020-11-26 10:18:45 +0100

    Mob programming : cleaning plugin

--- Koha/Plugin/Com/BibLibre/LinkedData.pm
+++ Koha/Plugin/Com/BibLibre/LinkedData.pm
@@ -4,26 +4,17 @@ use base qw(Koha::Plugins::Base);
 
 use Modern::Perl;
 
-use Encode qw( decode_utf8 );
-use File::Temp;
-
-use C4::Auth;
 use C4::Context;
-use Koha::Libraries;
 
-# TODO create a configuration page
-our $import_script = '/home/koha/bdp/scripts/import.pl';
-our $return_script = '/home/koha/bdp/scripts/nettoie_bdp.pl';
-
-our $VERSION = '1.0';
+our $VERSION = '0.1';
 
 our $metadata = {
-    name            => 'BDP Import',
+    name            => 'Linked data',
     author          => 'BibLibre',
-    description     => 'BDP Import via scripts',
-    date_authored   => '2020-09-01',
-    date_updated    => '2020-09-01',
-    minimum_version => '18.11',
+    description     => 'Linked data',
+    date_authored   => '2020-11-26',
+    date_updated    => '2020-11-26',
+    minimum_version => '19.11',
     maximum_version => undef,
     version         => $VERSION,
 };
@@ -68,71 +59,9 @@ sub tool {
     my $query = $self->{'cgi'};
 
     my $op = $query->param('op') || '';
-    my $test = $query->param('test');
-    my $branchcode = $query->param('branchcode');
-    my ( $filename, $script_logs );
-
-    if ( $op eq 'import' ) {
-        $filename = $query->param('bdpfile');
-        if ( -e $import_script) {
-
-            my $upload_filehandle = $query->upload('bdpfile');
-            my ( $tfh, $tempfile ) = File::Temp::tempfile( SUFFIX => '_import_bdp' );
-            binmode $tfh;
-            while (<$upload_filehandle>) {
-                print $tfh $_;
-            }
-
-            my @command;
-            push @command, $import_script;
-            push @command, "-s";
-            push @command, $branchcode;
-            push @command, "-file";
-            push @command, "$tempfile";
-            push @command, "-t " if ($test);
-
-            $script_logs = qx(@command);
-            $script_logs =~ s/\n/<br \/>/g;
-
-            close $tfh;
-        }
-        else {
-            $script_logs = "Le script $import_script n'existe pas";
-        }
-    }
-    elsif ( $op eq 'return' ) {
-        $filename = $query->param('returnbdpfile');
-        if ( -e $return_script) {
-            my $upload_filehandle = $query->upload('returnbdpfile');
-            my ( $tfh, $tempfile ) = File::Temp::tempfile( SUFFIX => '_return_bdp' );
-            binmode $tfh;
-            while (<$upload_filehandle>) {
-                print $tfh $_;
-            }
-
-            my @command;
-            push @command, $return_script;
-            push @command, "-file";
-            push @command, "$tempfile";
-            push @command, "-b";
-            push @command, C4::Context->userenv->{branch};
-            push @command, "-t" if ($test);
-            push @command, "-v";
-
-            $script_logs = qx(@command);
-            $script_logs =~ s/\n/<br \/>/g;
-
-            close $tfh;
-        }
-        else {
-            $script_logs = "Le script $return_script n'existe pas";
-        }
-    }
 
     $template->param(
-        filename    => $filename,
         op          => $op,
-        script_logs => Encode::decode_utf8($script_logs),
     );
 
     return $self->output_html( $template->output() );
